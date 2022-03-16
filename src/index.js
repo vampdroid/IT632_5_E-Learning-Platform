a2a4d5afed42d642752d658f9d5b010403c11649
jaykradadiya <jaykradadiya2001@gmail.com>
2022-03-16 10:40:40 +0530

    Login component

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import reportWebVitals from './reportWebVitals';
+import Login from "./Component/Login";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
