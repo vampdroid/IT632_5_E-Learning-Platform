ccf753e99ef7be62fdc75d60de0b5f1dfde07986
Mohit-Pandia <mohit.pandia26@gmail.com>
2022-05-09 14:43:44 +0530

    discussion api and CRUD

--- BackEnd/routes/discussion.js
+++ BackEnd/routes/discussion.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const { response } = require('express');
 let Discussion = require('../models/discussion.model');
 
 router.route('/add').post((req,res) => {
     const query = req.body.query;
     const course = req.body.course;
     const user = req.body.user;
-    const reply_id = req.body.reply_id;
     
-    const newDiscussion = new Discussion({query,course,user,reply_id});
+    const newDiscussion = new Discussion({query,course,user});
     newDiscussion.save()
         .then(() => res.json("Comment added"))
         .catch(err => res.status(400).json("error:"+err));
 });
+//////
 
+router.route('/readComment/:_id').get((req,res) => {
+    //according to course id find the response
+    const id = req.params._id;
+    
+    const comment = Discussion.find({course: id})
+        return res.status(200).json(comment)
+
+});
+
+router.route('/deleteComment/:_id').delete((req,res) => {
+    //according to course id find the response
+    const id = req.params._id;
+    
+    const comment = Discussion.deleteOne({_id: id})
+        return res.status(200).json("Deleted Succesfully")
+
+});
 module.exports = router;
