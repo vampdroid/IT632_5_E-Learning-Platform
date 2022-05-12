984640fb2bf9cc6c5be251678d113e0a5e42f70e
jaykradadiya <jaykradadiya2001@gmail.com>
2022-05-13 00:17:52 +0530

    mergeing

--- BackEnd/test/user.test.js
+++ BackEnd/test/user.test.js
@@ -3,12 +3,24 @@ const app = require('../app')
 
 test('Should create a new student',async ()=>{
     await request(app)
-        .post('/user')
+        .post("/user")
         .send({
             fname:"jay",
             lname:"radadiya",
             email:"jayradadiya81@gmail.com",
             password:"1234567",
             city:"surat"
-        }).except(200);
+        })
+        .except(200);
+})
+
+
+test('should login',async()=>{
+    await request(app)
+        .post('/user/login')
+        .send({
+            email:"jayradadiya81@gmail.com",
+            password:"1234567"
+        })
+        .except(200);
 })
